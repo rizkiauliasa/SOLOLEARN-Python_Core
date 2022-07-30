e4138347eab916e728cfd1048a646ac9f3e0be08
rizkiauliasa <rizkiauliasa@student.uns.ac.id>
2022-07-30 20:45:46 +0700

    2. Strings & Variables

---
+++ Simple Calculator.py
@@ -0,0 +1,19 @@
+# Simple Calculator
+
+
+# Write a program to take two integers as input and output their sum.
+
+# Sample Input:
+# 2
+# 8
+
+# Sample Output:
+# 10
+
+# Remember, input() results in a string.
+
+
+# your code goes here
+a = int(input())
+b = int(input ())
+print(a+b)
